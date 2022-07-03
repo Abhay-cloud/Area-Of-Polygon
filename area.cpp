9dbbcbf4a60d8e220661d281e4d5386d548ac01b
Abhay <54390973+Abhay-cloud@users.noreply.github.com>
2022-07-03 19:49:03 +0530

    updated code

--- area.cpp
+++ area.cpp
@@ -3,31 +3,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 double polygonArea(double X[], double Y[], int n)
 {
-	
-	double area = 0.0;
 
-	
-	int j = n - 1;
-	for (int i = 0; i < n; i++)
-	{
-		area += (X[j] + X[i]) * (Y[j] - Y[i]);
-		j = i; 
-	}
+    double area = 0.0;
 
-	
-	return abs(area / 2.0);
+    int j = n - 1;
+    for (int i = 0; i < n; i++)
+    {
+        area += (X[j] + X[i]) * (Y[j] - Y[i]);
+        j = i;
+    }
+
+    return abs(area / 2.0);
 }
-
 
 int main()
 {
-	double X[] = {0, 2, 4};
-	double Y[] = {1, 3, 7};
+    int m;
+    double X[] = {0, 2, 4};
+    double Y[] = {1, 3, 7};
+    double x[12], y[12];
 
-	int n = sizeof(X)/sizeof(X[0]);
+    cout << "Enter the no. of sides: ";
+    cin >> m;
 
-	cout << polygonArea(X, Y, n);
+    for (int i = 0; i < m; i++)
+    {
+        cout << "Enter the x" << i + 1 << ": ";
+        cin >> x[i];
+        cout << "Enter the y" << i + 1 << ": ";
+        cin >> y[i];
+    }
+
+    int n = sizeof(X) / sizeof(X[0]);
+
+    // cout << polygonArea(X, Y, n);
+    cout << polygonArea(x, y, m);
 }
